841ae93f9b2ee349e834973896b8000dafbc590b
Rajveer Kaur <30013158@student.toiohomai.ac.nz>
2019-08-22 10:49:28 +1200

    first

---
+++ WindowsForm/WindowsForm/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsForm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator c1 = new Calculator();
+            c1.num1 = 78.9;
+            c1.num2 = 12.9;
+            Console.WriteLine($"The sum of two numbers is: {c1.Add()}");
+            Console.ReadLine();
+        }
+    }
+    public class Calculator
+    {
+        public double num1 { get; set; }
+        public double num2 { get; set; }
+
+
+        public double Add()
+        { return num1 + num2; }
+
+        public double Sub()
+        { return num1 - num2; }
+
+        public double Multi()
+        { return num1 * num2; }
+
+        public double Divide()
+        { return num1 / num2; }
+    }
+}
---
+++ WindowsForm/WindowsForms-raj/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsForm;
+
+namespace WindowsForms_raj
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+            InitializeComponent();
+            Calculator c1 = new Calculator();
+            lblResult.Text = c1.num1.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
